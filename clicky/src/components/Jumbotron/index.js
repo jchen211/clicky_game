1bb16758bf7c973b39eb8ed121c213fbe0c207ae
Jessica Chen <jchen@jchen.attlocal.net>
2019-04-22 13:19:45 -0500

    changed class to className

--- clicky/src/components/Jumbotron/index.js
+++ clicky/src/components/Jumbotron/index.js
@@ -3,10 +3,10 @@ import './styles.css';
 
 function Jumbotron () {
         return (
-            <div class="jumbotron jumbotron-fluid">
-                <div class="container">
-                    <h1 class="display-4">CLICKY GAME!</h1>
-                    <p class="lead">Click the images to earn points. Don't click on the same twice. </p>
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1 className="display-4">CLICKY GAME!</h1>
+                    <p className="lead">Click the images to earn points. Don't click on the same twice. </p>
                 </div>
             </div>
         )
